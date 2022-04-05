bf6a6cb565f6cbb57a9afcb94db0dfac80a08fe9
todd1terry1 <84549472+todd1terry1@users.noreply.github.com>
2022-04-05 01:25:15 -0500

    front end file commit files

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static('public'));
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
